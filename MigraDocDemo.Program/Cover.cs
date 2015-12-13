ed06597f59cbbe0ec23d476bf71048e5e2990fcf
Guilherme Sonego Neto <gsonego@gmail.com>
2015-12-13 21:36:20 -0200

    Inclusao inicial

---
+++ MigraDocDemo.Program/Cover.cs
@@ -0,0 +1,31 @@
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Shapes;
+
+namespace MigraDocDemo.Program
+{
+    public class Cover
+    {
+        /// <summary>
+        /// Defines the cover page.
+        /// </summary>
+        public static void DefineCover(Document document)
+        {
+            Section section = document.AddSection();
+
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.SpaceAfter = "3cm";
+
+            Image image = section.AddImage("../../images/Logo.png");
+            image.Width = "10cm";
+
+            paragraph = section.AddParagraph("A sample document that demonstrates the\ncapabilities of MigraDoc");
+            paragraph.Format.Font.Size = 16;
+            paragraph.Format.Font.Color = Colors.DarkRed;
+            paragraph.Format.SpaceBefore = "8cm";
+            paragraph.Format.SpaceAfter = "3cm";
+
+            paragraph = section.AddParagraph("Rendering date: ");
+            paragraph.AddDateField();
+        }
+    }
+}
---
+++ MigraDocDemo.Program/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+
+namespace MigraDocDemo.Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            //SimpleDemo();
+
+            CompleteDemo();
+        }
+
+        static void CompleteDemo()
+        {
+            // Create a MigraDoc document
+            Document document = Documents.CreateDocument();
+
+            //string ddl = MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToString(document);
+            MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToFile(document, "MigraDoc.mdddl");
+
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer(true, PdfFontEmbedding.Always);
+            renderer.Document = document;
+
+            renderer.RenderDocument();
+
+            // Save the document...
+            const string filename = "HelloMigraDoc.pdf";
+            renderer.PdfDocument.Save(filename);
+
+            // ...and start a viewer.
+            Process.Start(filename);
+        }
+
+        private static void SimpleDemo()
+        {
+            // tudo comeca com um documento
+            var document = new Document();
+
+            // no documento nao se adiciona páginas e sim secoes
+            Section section = document.AddSection();
+
+            // adionamos um novo paragrafo à secao criada
+            section.AddParagraph("Hello, World!");
+
+            // adiciona paragrafo vazio
+            section.AddParagraph();
+
+            // cria um objeto paragrafo para modificacao
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+            paragraph.AddFormattedText("Hello, World!", TextFormat.Underline);
+
+            // também pode-se criar um objeto de texto formatado para ser manipulado
+            FormattedText ft = paragraph.AddFormattedText("Small text", TextFormat.Bold);
+            ft.Font.Size = 6;
+
+            // podemos renderizar o documento em PDF ou RTF
+            var pdfRenderer = new PdfDocumentRenderer(false, PdfFontEmbedding.Always);
+
+            // basta passar o documento criado anteriormente ao renderizador
+            pdfRenderer.Document = document;
+
+            // solicita a renderizacao
+            pdfRenderer.RenderDocument();
+
+            // salva o arquivo gerado em disco
+            const string filename = "HelloWorld.pdf";
+            pdfRenderer.PdfDocument.Save(filename);
+
+            // solicita a abertura do arquivo
+            Process.Start(filename);
+        }
+    }
+}
